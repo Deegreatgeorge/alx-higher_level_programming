a56ca8b25ccedd8e4d09b6c65b58c6e50400a6ee
Deegreatgeorge <madukag0@gmail.com>
2023-11-27 16:05:59 -0800

    additions

---
+++ 0x00-python-hello_world/102-magic_calculation.py
@@ -0,0 +1,12 @@
+def magic_calculation(a, b):
+    # 0 LOAD_CONST               1 (98)
+    # 3 LOAD_FAST                0 (a)
+    # 6 LOAD_FAST                1 (b)
+    # 9 BINARY_POWER
+    # 10 BINARY_ADD
+    # 11 RETURN_VALUE
+    if a < b:
+        return b + a
+    else:
+        return 98
+
