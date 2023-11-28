daea25cd92f8453cc83c2a53cce14f272845bb29
Deegreatgeorge <madukag0@gmail.com>
2023-11-27 16:16:08 -0800

    Additions

--- 0x00-python-hello_world/102-magic_calculation.py
+++ 0x00-python-hello_world/102-magic_calculation.py
@@ -1,12 +1,14 @@
 def magic_calculation(a, b):
     # 0 LOAD_CONST               1 (98)
-    # 3 LOAD_FAST                0 (a)
-    # 6 LOAD_FAST                1 (b)
-    # 9 BINARY_POWER
-    # 10 BINARY_ADD
-    # 11 RETURN_VALUE
+    # 2 LOAD_FAST                0 (a)
+    # 4 LOAD_FAST                1 (b)
+    # 6 COMPARE_OP               0 (<)
+    # 8 POP_JUMP_IF_FALSE       16
+    # 10 LOAD_FAST                1 (b)
+    # 12 LOAD_FAST                0 (a)
+    # 14 BINARY_ADD
+    # 16 RETURN_VALUE
     if a < b:
         return b + a
     else:
         return 98
-
